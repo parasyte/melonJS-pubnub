876af08cdd10ce53748a75cc765f98f1021addf7
Jay Oster <jay@pubnub.com>
2017-03-31 17:45:39 -0700

    Update melonJS and platformer demo

--- js/resources.js
+++ js/resources.js
@@ -1,48 +1,59 @@
 game.resources = [
 
-	/* Graphics. 
-	 * @example
-	 * {name: "example", type:"image", src: "data/img/example.png"},
-	 */
-	{name: "tileset",		type:"image",	src: "data/img/tileset.png"},
-	{name: "atascii",		type:"image",	src: "data/img/atascii_24px.png"},
-	{name: "background",	type:"image",	src: "data/img/background.png"},	
-	
-		
-	/* Maps. 
-	 * @example
-	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
-	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
- 	 */
-	{name: "map1",			type: "tmx",	src: "data/map/map1.tmx"},
-	{name: "map2",			type: "tmx",	src: "data/map/map2.tmx"},
+    /* Graphics.
+     * @example
+     * { name: "example", type:"image", src: "data/img/example.png" },
+     */
+    { name: "tileset",         type:"image",   src: "data/img/tileset.png" },
+    { name: "background",      type:"image",   src: "data/img/background.png" },
+    { name: "clouds",          type:"image",   src: "data/img/clouds.png" },
+
+    /* Maps.
+     * @example
+     * { name: "example01", type: "tmx", src: "data/map/example01.tmx" },
+     * { name: "example01", type: "tmx", src: "data/map/example01.json" },
+     */
+    { name: "map1",            type: "tmx",    src: "data/map/map1.tmx" },
+    { name: "map2",            type: "tmx",    src: "data/map/map2.json" },
 
 
-	/* Background music. 
-	 * @example
-	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
-	 */	
-	{name: "DST-GameForest",type: "audio",	src: "data/bgm/",	channel : 1},
-	
-	/* Sound effects. 
-	 * @example
-	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
-	 */
-	{name: "cling",			type: "audio",	src: "data/sfx/",	channel : 2},
-	{name: "die",			type: "audio",	src: "data/sfx/",	channel : 1},
-	{name: "enemykill",		type: "audio",	src: "data/sfx/",	channel : 1},
-	{name: "jump",			type: "audio",	src: "data/sfx/",	channel : 2},
+    /* Tilesets.
+     * @example
+     * { name: "example01", type: "tsx", src: "data/map/example01.tsx" },
+     * { name: "example01", type: "tsx", src: "data/map/example01.json" },
+     */
+    { name: "tileset",         type: "tsx",    src: "data/map/tileset.json" },
 
-	
-	/* Atlases 
-	 * @example
-	 * {name: "example_tps", type: "json", src: "data/img/example_tps.json"},
-	 */
-	// texturePacker
-	{name: "texture",		type: "json",	src: "data/img/texture.json"},
-	{name: "texture",		type: "image",	src: "data/img/texture.png"}
-	// ShoeBox
-	//{name: "texture",		type: "json",	src: "data/gfx/shoebox.json"},
-	//{name: "texture",		type:"image",	src: "data/gfx/shoebox.png"}
-	 
+
+    /* Background music.
+     * @example
+     * { name: "example_bgm", type: "audio", src: "data/bgm/" },
+     */
+    { name: "dst-gameforest",  type: "audio",  stream: true, src: "data/bgm/" },
+
+    /* Sound effects.
+     * @example
+     * { name: "example_sfx", type: "audio", src: "data/sfx/" }
+     */
+    { name: "cling",           type: "audio",  src: "data/sfx/" },
+    { name: "die",             type: "audio",  src: "data/sfx/" },
+    { name: "enemykill",       type: "audio",  src: "data/sfx/" },
+    { name: "jump",            type: "audio",  src: "data/sfx/" },
+
+
+    /* Atlases
+     * @example
+     * { name: "example_tps", type: "json", src: "data/img/example_tps.json" },
+     */
+    // texturePacker
+    { name: "texture",         type: "json",   src: "data/img/texture.json" },
+    { name: "texture",         type: "image",  src: "data/img/texture.png" },
+
+    /* Bitmap Font
+    * @example
+    * { name: "example_fnt", type: "image", src: "data/img/example_fnt.png" },
+    * { name: "example_fnt", type: "binary", src: "data/img/example_fnt.fnt" },
+    */
+    { name: "PressStart2P", type:"image", src: "data/fnt/PressStart2P.png" },
+    { name: "PressStart2P", type:"binary", src: "data/fnt/PressStart2P.fnt"}
 ];
--- js/screens/play.js
+++ js/screens/play.js
@@ -1,45 +1,31 @@
 game.PlayScreen = me.ScreenObject.extend({
-	/**	
-	 *  action to perform on state change
-	 */
-	onResetEvent: function() {	
+    /**
+     *  action to perform on state change
+     */
+    onResetEvent: function() {
       // load a level
-		me.levelDirector.loadLevel("map1");
-		
-		// reset the score
-		game.data.score = 0;
-		
-		// add our HUD to the game world	
-		me.game.add(new game.HUD.Container());
-		
-		// play some music
-		me.audio.playTrack("DST-GameForest");
+        me.levelDirector.loadLevel("map1");
 
-		// Instantiate the Multiplayer object
-		game.mp = new Multiplayer(function (x, y) {
-		// Create a new player object
-		var obj = me.entityPool.newInstanceOf("mainplayer", x, y, {
-			spritewidth : 72,
-			spriteheight : 98,
-			isMP : true
-		});
+        // reset the score
+        game.data.score = 0;
 
-		me.game.add(obj, 4);
-		me.game.sort();
+        // add our HUD to the game world
+        this.HUD = new game.HUD.Container();
+        me.game.world.addChild(this.HUD);
 
-		return obj;
-		});
-	},
-	
-	/**	
-	 *  action to perform on state change
-	 */
-	onDestroyEvent: function() {	
-	
-		// remove the HUD from the game world
-		me.game.world.removeChild(me.game.world.getEntityByProp("name", "HUD")[0]);
-		
-		// stop some music
-		me.audio.stopTrack("DST-GameForest");
-	}
+        // play some music
+        me.audio.playTrack("dst-gameforest");
+    },
+
+    /**
+     *  action to perform on state change
+     */
+    onDestroyEvent: function() {
+
+        // remove the HUD from the game world
+        me.game.world.removeChild(this.HUD);
+
+        // stop some music
+        me.audio.stopTrack("dst-gameforest");
+    }
 });
